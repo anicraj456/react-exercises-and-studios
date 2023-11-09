9b762a8d752f40106d960ac34732495b39c135be
anicraj456 <anicraj123@gmail.com>
2023-11-08 19:16:10 -0600

    part 1 completed

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book Must Read In 2023";
+   let book1 = "https://m.media-amazon.com/images/I/81KNNeRJdFL._SY522_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/91RfqnfA5hL._SY522_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/81zi7dJjl8L._SY522_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Falling Stars By Fern Michaels " />
+         <img src={book2} alt="No Strangers Here: A Riveting Irish Thriller By Carlene O’Connor " />
+         <img src={book3} alt="Santa and Company (Santa's Crew) By Fern Michaels" />
       </div>      
    );
 }
