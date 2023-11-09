d77a926f7e123dc35d57966654d250db4adcc699
anicraj456 <anicraj123@gmail.com>
2023-11-09 06:00:47 -0600

    workig on studio

--- part1/studio/src/App.jsx
+++ part1/studio/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import RecipeDescription from './components/Description.jsx'
+import RecipeIngredients from './components/Ingredients.jsx'
+import RecipePhoto from './components/Photos.jsx'
 
 function App() {
 
