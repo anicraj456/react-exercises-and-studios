6bdb89df1344f8b69322d81ba2ccc910f72a6849
anicraj456 <anicraj123@gmail.com>
2023-11-08 22:32:20 -0600

    working on stuio part 1

---
+++ part1/studio/src/components/Photos.jsx
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+export default function RecipePhoto(){
+    return(
+        <img src="valid URL to recipe photo" alt="recipe photo" className = {styles.imageUpdates} />
+    )
+}
