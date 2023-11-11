62509430e73db1084de05be7ecad68799e8a32ab
anicraj456 <anicraj123@gmail.com>
2023-11-11 10:31:05 -0600

    part 3 exercise completed

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Sewing</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Stress Relief. Sewing encourages mindfullness and reduces stress and anxiety.</li>
+            <li>Improves hand-eye coordination. </li>
+            <li>Hand stitching, machine sewing, serging, and overlocking </li>
          </ol>
       </div>
    );
